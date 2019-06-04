1905c7d6eae54da3fa7352e3497e458da298d6c2
ADI25 <ADI25@10.10.4.87>
2019-06-04 13:56:17 +0300

    window size in Main change

--- gcm_project_compatible/src/application/Main.java
+++ gcm_project_compatible/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/application/LogIn.fxml"));
-			Scene scene = new Scene(root,700,600);
+			Scene scene = new Scene(root,500,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
